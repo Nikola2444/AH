7892e50049170bbd00a41146479fc0e0c129e974
Nikola <krajka95@gmail.com>
2019-05-13 20:16:21 +0200

    zad1 and zad2 probably done

---
+++ zad1/zad1.m
@@ -0,0 +1,96 @@
+clear
+clc
+%% ZAD1  dvo-ulazni multiplekser
+% training data
+x1_training_data = [0 0 0 0 1 1 1 1 1 0 1 0 1 1 0]; 
+x2_training_data = [0 0 1 1 0 0 1 1 0 0 1 0 0 0 1];
+sel =              [0 1 0 1 0 1 0 1 1 0 1 1 0 1 1];
+y=                 [0 0 0 1 1 0 1 1 0 0 1 0 1 0 1];
+
+x_training_data = [x1_training_data;x2_training_data;sel];
+
+% Testing data
+x1_test = [0, 0, 1, 1 1 1]
+x2_test = [0, 1, 1, 1 0 0]
+sel_test = [0, 1, 0 ,1 0 1]
+
+x_test = [x1_test;x2_test;sel_test];    
+
+
+[Y,Xf,Af] = mux_function(x_test)
+
+%~a ~c ~d + b ~c ~d + a ~b ~c + ~a b ~c d + ~a ~b c ~d
+%~x[1] *~x[3]* ~x[4] + x[2] * ~x[3] *~x[4] + x[1] * ~x[2] *~x[3] + ~x[1]* x[2] *~x[3] * x[4] + ~x[1] * ~x[2] *x[3]*~x[4]
+%~a ~b ~d + ~c ~d + ~a b ~c + a ~b ~c
+
+%% ZAD1  cetvoro-ulazni multiplekser
+x1_training_data(1) = 0;
+x2_training_data(1) = 0;
+x3_training_data(1) = 0;
+x4_training_data(1) = 0;
+sel1(1) = 0;
+sel2(1) = 0;
+for i = 1:150
+    x1_training_data(i+1) = mod(i, 2);
+    if (mod(i, 2) == 0)
+        x2_training_data(i+1) = xor(x2_training_data(i), 1);
+    else
+        x2_training_data(i+1) = x2_training_data(i);
+    end
+    if (mod(i, 4) == 0)
+        x3_training_data(i+1) = xor(x3_training_data(i), 1);
+    else
+        x3_training_data(i+1) = x3_training_data(i);
+    end
+    
+    if (mod(i, 8) == 0  && i ~= 0)
+        x4_training_data(i+1) = xor(x4_training_data(i), 1);
+    else
+        x4_training_data(i+1) = x4_training_data(i);
+    end
+    
+    if (mod(i, 16) == 0  && i ~= 0)
+        sel1(i+1) = xor(sel1(i), 1);
+    else
+        sel1(i+1) = sel1(i);
+    end
+    
+    if (mod(i, 32) == 0  && i ~= 0)
+        sel2(i+1) = xor(sel2(i), 1);
+    else
+        sel2(i+1) = sel2(i);
+    end
+    
+    
+    
+end
+
+for i = 1 : 151
+    if (sel1(i) == 0 && sel2(i) == 0)
+        y(i) = x1_training_data(i);
+    elseif (sel1(i) == 1 && sel2(i) == 0)
+        y(i) = x2_training_data(i);
+    elseif (sel1(i) == 0 && sel2(i) == 1)
+          y(i) = x3_training_data(i);
+    else 
+          y(i) = x4_training_data(i);
+    end
+end
+
+x = [x1_training_data;x2_training_data;x3_training_data;x4_training_data;sel1;sel2];
+
+
+x1_test = [0, 0, 1, 1 1 1]
+x2_test = [0, 1, 1, 1 0 0]
+x3_test = [1, 0, 0, 1 0 1]
+x4_test = [1, 1, 1, 1 0 1]
+sel1_test = [1, 0, 0 ,1 0 1]
+sel2_test = [0, 1, 0 ,1 0 1]
+x_test = [x1_test;x2_test;x3_test;x4_test;sel1_test;sel2_test];
+
+[Y,Xf,Af] = myNeuralNetworkFunction(x_test)
+
+
+
+
+
---
+++ zad2/zad2.m
@@ -0,0 +1,62 @@
+%% R kolo zad1 
+clc
+clear
+syms I1 I2 r
+
+
+S = [10 0 0 -4; 0 16 -4 0; 0 -4 10 0; -4 0 0 16]
+Y = [(26 + r); (34 + 2*r); (31- 3*r); (38 -2*r)];
+
+S_inv = inv(S);
+
+X = S_inv * Y
+
+I1 = [X(1) X(3)]
+I2 = [X(2) X(4)]
+
+
+%% RLC kolo zad2
+
+A = [10 -6; -6 16]
+B = [-7.5 5; 5 3];
+
+U = [(4 + r) (6 - r); (-2 + r) -r]
+V = [(-1 + r) (1 - r); (-3 + r) (-1 - r)]
+
+S1_A = A;
+S1_A(A < 0) = 0
+ 
+S1_B = B;
+S1_B(B < 0) = 0
+
+
+S2_A = A;
+S2_A (A > 0) = 0
+
+S2_B = B;
+S2_B (B > 0) = 0
+
+S1 = [S1_A -S2_B; S1_B S1_A]
+S2 = [S2_A -S1_B; S2_B S2_A]
+
+S = [S1 S2; S2 S1]
+Y = [(4 + r); (-2 + r); (-1 + r); (-3 + r); (6 - r); (-r); (1 - r); ...
+    (-1 - r)]
+S_inv = inv(S);
+I = vpa(S_inv * Y, 5)
+
+I1_real = vpa([I(1), I(5)], 4)
+r = 1;
+I1_params_real = vpa(eval(I1_real),4)
+
+I1_imag = vpa([I(3), I(7)], 4)
+I1_params_imag = vpa(eval(I1_imag), 4)
+
+I2_real = vpa([I(2), I(6)], 4)
+I2_params_real = vpa(eval(I2_real), 4)
+
+I2_imag = vpa([I(4), I(8)], 4)
+I2_params_imag = vpa(eval(I2_imag), 4)
+
+vpa(eval(U),4)
+vpa(eval(U),4)
